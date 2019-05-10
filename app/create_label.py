40821aa9894419cb54e72f28364d4e0e3ea6d2bb
Jonathan Mucha <jonmucha@gmail.com>
2019-05-10 13:33:56 -0400

    working out create_label file logistics

--- app/create_label.py
+++ app/create_label.py
@@ -1,3 +1,22 @@
+from reportlab.lib.pagesizes import letter, inch, landscape
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
+from reportlab.platypus import Image
+import reportlab.rl_config
+reportlab.rl_config.warnOnMissingFontGlyphs = 0
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+import os
+
+label_page_size = (8*inch, 2.5*inch)
+PAGE_HEIGHT=label_page_size[1]
+PAGE_WIDTH=label_page_size[0]
+basedir = os.path.abspath(os.path.dirname(__file__)) # put the files in the right place no matter what!
+pdfmetrics.registerFont(TTFont('Monsterrat', basedir+'/Montserrat/Montserrat-Regular.ttf'))
+
+
+
+
 class Label():
 	def __init__(self,):
 		self.collection = ''
@@ -5,13 +24,47 @@ class Label():
 		self.order = ''
 		self.num = ''
 		self.date = ''
+		self.path = basedir+'/labels/'
 
 	def generate_label(self,collection, color, order, num, date):
 		self.collection = collection
 		self.color = color
-		self.order = order
+		self.order = order # must configure
 		self.num = num
 		self.date = date
+		self.path = self.path + self.collection+'_'+self.color+'.pdf'
+		label_canvas = canvas.Canvas(self.path,pagesize=label_page_size)
 
-		print(self.collection, self.color, self.order)
-		return(self)
+		# Set Label font and sizing & translate
+		label_canvas.setLineWidth(0.3)
+		label_canvas.setFont('Monsterrat', 36)
+		label_canvas.translate(PAGE_WIDTH/2.0,PAGE_HEIGHT/2.0)
+
+		# Draw RW Logo
+		im = Image(basedir+'/final_logo_TM.jpg', width=3*inch, height=1*inch)
+		im.drawOn(label_canvas,-1.5*inch,0.25*inch)
+
+		# Draw the Collection Name and SKU Name
+		label_canvas.drawString(-PAGE_WIDTH/2.0 + 0.25*inch, \
+		                        PAGE_HEIGHT/2.0-(1.5*inch), \
+		                        self.collection)
+
+		label_canvas.drawString(-PAGE_WIDTH/2.0 + 0.25*inch, \
+		                        PAGE_HEIGHT/2.0-(1.5*inch) - 46,\
+		                        self.color)
+
+		# Draw labels for production date and run number
+		label_canvas.setFont('Monsterrat', 16)
+
+		label_canvas.drawString(PAGE_WIDTH/2.0 - 3.5*inch, \
+		                        PAGE_HEIGHT/2.0-(1.75*inch)-18, \
+		                        self.date)
+
+		label_canvas.drawString(PAGE_WIDTH/2.0 - 3.5*inch, \
+		                        PAGE_HEIGHT/2.0-(2*inch)-18, \
+		                        'Run #: '+self.num)
+
+
+
+		label_canvas.save()
+		return(self.path)
